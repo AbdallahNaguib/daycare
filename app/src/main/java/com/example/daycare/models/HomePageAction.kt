19604d13206b1630815b0b6c2f96678301657a5a
abdo <abdonaguib99@gmail.com>
2021-11-21 00:42:20 +0200

    finished homepage for parent

---
+++ app/src/main/java/com/example/daycare/models/HomePageAction.kt
@@ -0,0 +1,5 @@
+package com.example.daycare.models
+
+import android.graphics.drawable.Drawable
+
+data class HomePageAction(val icon: Int, val mainLabel: String)
---
+++ app/src/main/java/com/example/daycare/parent/adapters/HomePageAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.daycare.parent.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.daycare.R
+import com.example.daycare.models.HomePageAction
+
+class HomePageAdapter(private val homepageActions:List<HomePageAction>) : RecyclerView.Adapter<HomePageAdapter.ViewHolder>() {
+    class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val icon:ImageView = view.findViewById(R.id.icon)
+        val mainLabel:TextView = view.findViewById(R.id.mainLabel)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.parent_homepage_list_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = homepageActions[position]
+        holder.icon.setBackgroundResource(item.icon)
+        holder.mainLabel.text = item.mainLabel
+    }
+
+    override fun getItemCount() = homepageActions.size
+}
